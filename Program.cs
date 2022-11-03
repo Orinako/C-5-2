623e2ed03ade41ba2583235b8789696b16b960f0
Anna Podchasova <pod9696@gmail.com>
2022-11-03 15:03:07 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Задание_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string sentence = Console.ReadLine();
+                        
+            ReverseSentense(sentence);
+
+            Console.ReadKey();
+        }
+
+        static string[] SplitSentence(string text)
+        {
+            string[] wordsArray = text.Split(' ');
+            return wordsArray;
+        }
+
+        static void ReverseSentense(string text)
+        {
+            string[] wordsArray = SplitSentence(text);
+                       
+            Array.Reverse(wordsArray);
+            int i = 0;
+            while (i++ < wordsArray.Length)
+            {
+                Console.Write($"{wordsArray[i - 1]} ");
+            }
+        }
+    }
+}
